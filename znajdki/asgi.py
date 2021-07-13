bfdd034bf2b790ea1af4cf3818f27de28578b52f
Aleooo <aleksander.wiedenski@gmail.com>
2021-07-13 22:17:38 +0200

    bugs in settings

--- znajdki/asgi.py
+++ znajdki/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'znajdki.production_setting')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'znajdki.settings')
 
 application = get_asgi_application()
--- znajdki/wsgi.py
+++ znajdki/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'znajdki.production_setting')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'znajdki.settings')
 
 application = get_wsgi_application()
