b1b62cc4b814af6324b25938fc3f3e9c02a0aaaf
Aleooo <aleksander.wiedenski@gmail.com>
2021-07-24 21:44:00 +0200

    docker 1

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,8 +1,10 @@
+#!/bin/sh
+
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
 
-    while ! nc -z $DB_HOST; do
+    while ! nc -z $DB_HOST 5432; do
       sleep 0.1
     done
 
