411380557ae3840b7c80d279ae86a7ebb19bdea0
Aleooo <aleksander.wiedenski@gmail.com>
2021-07-28 23:00:09 +0200

    docker fixed gunicorn bug

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-echo "Waiting for postgres..."
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
 
-while ! nc -z $DB_HOST 5432; do
-  sleep 0.1
-done
+    while ! nc -z $DB_HOST 5432; do
+      sleep 0.1
+    done
 
-echo "PostgreSQL started"
+    echo "PostgreSQL started"
+fi
+
+exec "$@"
